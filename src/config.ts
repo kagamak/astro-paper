18347547ecbf6c90a68b2124c4481ca77ef88eba
kagamak <88057312+kagamak@users.noreply.github.com>
2023-05-01 13:29:48 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://emelien.com/",
+  author: "Emelie Nguyen",
+  desc: "Emelie Nguyen's Blog",
+  title: "Home",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
